ece7c3327c262d97bc910faedafaef108e87f91e
Anton Galichin <galichin-anton@yandex.ru>
2020-06-11 13:14:10 +0800

    Chapter 7

--- src/StudentOrderValidator.java
+++ src/StudentOrderValidator.java
@@ -3,26 +3,43 @@ public class StudentOrderValidator {
         checkAll();
     }
 
-    static void checkCityRegister() {
-
-    }
-
-    static void checkWedding() {
-
-    }
-
-    static void checkChildren() {
-
-    }
-
-    static void checkStudent() {
-
-    }
-
     static void checkAll() {
-        checkCityRegister();
-        checkWedding();
-        checkChildren();
-        checkStudent();
+        StudentOrder studentOrder = readStudentOrder();
+
+        AnswerCityRegister cityAnswer = checkCityRegister(studentOrder);
+        AnswerWedding wedAnswer = checkWedding(studentOrder);
+        AnswerChildren childAnswer = checkChildren(studentOrder);
+        AnswerStudent studentAnswer = checkStudent(studentOrder);
+
+        sendMail(studentOrder);
+    }
+
+    static StudentOrder readStudentOrder() {
+        StudentOrder studentOrder = new StudentOrder();
+        return studentOrder;
+    }
+
+    static AnswerCityRegister checkCityRegister(StudentOrder studentOrder) {
+        AnswerCityRegister answer = new AnswerCityRegister();
+        return answer;
+    }
+
+    static AnswerWedding checkWedding(StudentOrder studentOrder) {
+        AnswerWedding answer = new AnswerWedding();
+        return answer;
+    }
+
+    static AnswerChildren checkChildren(StudentOrder studentOrder) {
+        AnswerChildren answer = new AnswerChildren();
+        return answer;
+    }
+
+    static AnswerStudent checkStudent(StudentOrder studentOrder) {
+        AnswerStudent answer = new AnswerStudent();
+        return answer;
+    }
+
+    static void sendMail(StudentOrder studentOrder) {
+
     }
 }
