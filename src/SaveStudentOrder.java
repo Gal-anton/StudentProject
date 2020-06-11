34e16db55e8b4f9179832151a34b3ffdd6894128
Anton Galichin <galichin-anton@yandex.ru>
2020-06-11 11:55:35 +0800

    Chapter 6

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,9 +1,18 @@
 public class SaveStudentOrder {
     public static void main(String[] args) {
-        saveStudentOrder();
+        StudentOrder so = new StudentOrder();
+        so.hFirstName = "Aleksey";
+        so.hLastName = "Petrov";
+        so.wFirstName = "Daria";
+        so.wLastName = "Petrova";
+
+        long answer = saveStudentOrder(so);
+        System.out.println(answer);
     }
 
-    static void saveStudentOrder() {
-        System.out.println("Save is running!");
+    static long saveStudentOrder(StudentOrder studentOrder) {
+        long answer = 199;
+        System.out.println("SaveStudentOrder: " + studentOrder.hLastName);
+        return answer;
     }
 }
--- src/StudentOrderValidator.java
+++ src/StudentOrderValidator.java
@@ -1,5 +1,28 @@
 public class StudentOrderValidator {
     public static void main(String[] args) {
+        checkAll();
+    }
 
+    static void checkCityRegister() {
+
+    }
+
+    static void checkWedding() {
+
+    }
+
+    static void checkChildren() {
+
+    }
+
+    static void checkStudent() {
+
+    }
+
+    static void checkAll() {
+        checkCityRegister();
+        checkWedding();
+        checkChildren();
+        checkStudent();
     }
 }
